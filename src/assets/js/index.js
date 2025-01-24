5c0df4ce9d8823d7596651f87417a5893ccd8320
Riho yamamoto <r.yamamoto@rhino-inc.jp>
2025-01-24 16:25:46 +0900

    scrollsnapとanchorの調整

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -18,17 +18,20 @@ async function opning() {
     $body.addClass('noscroll');
     await tick(5000);
     $('.mv_wrap').fadeOut(1000);
-    $body.removeClass('noscroll');
     await tick(300);
     $('.contents').addClass('show');
 }
 
 $('.skip').on('click', function () {
     $('.mv_wrap').fadeOut(1000);
-    $body.removeClass('noscroll');
     $('.contents').addClass('clickshow');
 });
 
+$(window).on('scroll', function() {
+      $('.mv_wrap').fadeOut(1000, function() {
+      $('.contents').addClass('clickshow');
+  });
+});
 
 
 
@@ -46,25 +49,66 @@ const swiper = new Swiper('.swiper', {
     $(this).addClass('show');
     $('.nav_feed').addClass('hidden'); 
     $('.overlay').addClass('show'); 
+    
     $body.addClass('noscroll');
 });
 $('.nav_feed').on('click', function () {
     $(this).removeClass('hidden');
     $('.nav_index').removeClass('show'); 
     $('.overlay').removeClass('show'); 
+    
     $body.removeClass('noscroll');
 });
 
-// アンカーリンク
-$(window).on('load', function () {
-    $('.anchor_link a[href*="#"]').on('click', function () {
-      var elmHash = $(this).attr('href');
-      var pos = $(elmHash).offset().top;
-      $('html, body').animate({ scrollTop: pos }, 1);
-      $('.nav_feed').removeClass('hidden'); 
-      $('.nav_index').removeClass('show'); 
-        $('.overlay').removeClass('show'); 
-        $body.removeClass('noscroll');
-      return false;
+$(window).on('scroll', function () {
+    styleOn();
+});
+
+
+function styleOn() {
+    $('.item').each(function () {
+        var element = $(this);
+        var elementTop = element.offset().top;
+        var windowTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        // 要素の上部がウィンドウの60%位置に来たときにクラスを追加
+        if (windowTop + windowHeight * 0.6 > elementTop) {
+            element.addClass('on');
+            element.addClass('active');
+        } else {
+            element.removeClass('on');
+        }
     });
-  });
+}
+
+
+$(window).on('load', function () {
+    $('.anchor_link a[href*="#"]').on('click', function (e) {
+        e.preventDefault(); // デフォルトのアンカーリンクの動作を防ぐ
+        var elmHash = $(this).attr('href');
+        var target = $(elmHash);
+        
+        if (target.length) {
+            // スクロールスナップを一時的に無効にする
+            $('.sp-main').css('scroll-snap-type', 'none');
+
+            // スクロール位置を計算
+            var pos = target.position().top + $('.sp-main').scrollTop();
+
+            // 一瞬でスクロールする
+            $('.sp-main').scrollTop(pos);
+
+            // スクロールが完了した後にスクロールスナップを再度有効にする
+            $('.sp-main').css('scroll-snap-type', 'y mandatory');
+
+            // その他の処理
+            $('.nav_feed').removeClass('hidden'); 
+            $('.nav_index').removeClass('show'); 
+            $('.overlay').removeClass('show'); 
+            $body.removeClass('noscroll');
+        }
+
+        return false; // イベントのデフォルト動作を防ぐ
+    });
+});
